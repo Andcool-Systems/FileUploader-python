e41ed5a5aba4e02e98384c2578748587314035aa
AndcoolSystems <drolll2007@gmail.com>
2024-07-16 01:18:16 +0300

    Update account.py

--- fileuploader/tests/account.py
+++ fileuploader/tests/account.py
@@ -4,8 +4,8 @@ import asyncio
 
 async def run():
     user = await fileuploader.User.register("Username", "Password")  # Create an account with passed username and password
-    #user = await fileuploader.User.login("Username", "Password")  # Log in by username and password
-    #user = await fileuploader.User.loginToken("<token>")  # You can also log in with an access token
+    # user = await fileuploader.User.login("Username", "Password")  # Log in by username and password
+    # user = await fileuploader.User.loginToken("<token>")  # You can also log in with an access token
     print(user)
 
 
